14f87bcefcd866a121516f6b06ad6418889c8d28
Alena <alenabelovadmvn@gmail.com>
2025-03-19 11:22:02 -0400

    page categories and the display were fixed

--- BudgetBuddy/View/CategoriesView.swift
+++ BudgetBuddy/View/CategoriesView.swift
@@ -12,22 +12,71 @@ struct CategoriesView: View {
     @State private var allCategories: [Category] = []
     @State private var addCategory: Bool = false
     @State private var categoryName: String = ""
+    @State private var deleteRequest: Bool = false
+        @State private var requestedCategory: Category?
 
     let db = Firestore.firestore()
+    
+    var sortedCategories: [Category] {
+            allCategories.sorted { $0.transactions.count > $1.transactions.count }
+        }
 
     var body: some View {
         NavigationStack {
             List {
-                ForEach(allCategories) { category in
-                    Text(category.categoryName)
-                }
+                ForEach(sortedCategories) { category in
+                                    DisclosureGroup {
+                                        if !category.transactions.isEmpty {
+                                            ForEach(category.transactions.compactMap { $0 as? Expense }) { expense in
+                                                TransactionCardView(expense: expense, displayTag: false)
+                                            }
+                                        } else {
+                                            ContentUnavailableView {
+                                                Label("No Expenses", systemImage: "tray.fill")
+                                            }
+                                        }
+                                    } label: {
+                                        Text(category.categoryName)
+                                    }
+                                    .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                                        Button {
+                                            deleteRequest.toggle()
+                                            requestedCategory = category
+                                        } label: {
+                                            Image(systemName: "trash")
+                                        }
+                                        .tint(.red)
+                                    }
+                                }
             }
+            .navigationTitle("Categories")
+                        .overlay {
+                            if allCategories.isEmpty {
+                                ContentUnavailableView {
+                                    Label("No Categories", systemImage: "tray.fill")
+                                }
+                            }
+                        }
+                        .alert("If you delete a category, all associated expenses will be deleted too.", isPresented: $deleteRequest) {
+                                    Button(role: .destructive) {
+                                        if let requestedCategory {
+                                            allCategories.removeAll { $0.id == requestedCategory.id }
+                                            self.requestedCategory = nil
+                                        }
+                                    } label: {
+                                        Text("Delete")
+                                    }
+                                    Button(role: .cancel) { requestedCategory = nil } label: {
+                                        Text("Cancel")
+                                    }
+                                }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
                         addCategory.toggle()
                     } label: {
                         Image(systemName: "plus.circle.fill")
+                            .font(.title3)
                     }
                 }
             }
@@ -54,6 +103,9 @@ struct CategoriesView: View {
                         }
                     }
                 }
+                .presentationDetents([.height(180)])
+                .presentationCornerRadius(20)
+                .interactiveDismissDisabled()
             }
             .onAppear {
                 fetchCategories()
